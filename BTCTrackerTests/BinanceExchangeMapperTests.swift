23fceff2d9d6d181370a541ff34b643bb1036b6e
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-10 15:13:29 -0500

    Add HTTPURLResponse param to mapper

--- BTCTrackerTests/BinanceExchangeMapperTests.swift
+++ BTCTrackerTests/BinanceExchangeMapperTests.swift
@@ -12,7 +12,7 @@ final class BinanceExchangeMapper {
         case invalidData
     }
 
-    static func map(_ data: Data) throws {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws {
         throw Error.invalidData
     }
 }
@@ -22,13 +22,19 @@ final class BinanceExchangeMapperTests: XCTestCase {
     func test_map_withInvalidJSON_throwsError() {
         let data = Data("invalid json".utf8)
 
-        XCTAssertThrowsError(try BinanceExchangeMapper.map(data))
+        XCTAssertThrowsError(try BinanceExchangeMapper.map(data, from: anyHTTPURLResponse()))
     }
 
     func test_map_withMalformedJSON_throwsError() {
         let data = Data("{ \"ticker\": \"BTCUSDT\" }".utf8)
 
-        XCTAssertThrowsError(try BinanceExchangeMapper.map(data))
+        XCTAssertThrowsError(try BinanceExchangeMapper.map(data, from: anyHTTPURLResponse()))
+    }
+
+    // MARK: - Helpers
+    private func anyHTTPURLResponse() -> HTTPURLResponse {
+        let url = URL(string: "http://any-url.com")!
+        return HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
     }
 
 }
