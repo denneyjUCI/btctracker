28cc222025bf5584cce9e5379ec8b539e8077d6a
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-10 13:29:31 -0500

    Add Exchange model

---
+++ BTCTracker/Exchange.swift
@@ -0,0 +1,13 @@
+//
+//  Exchange.swift
+//  BTCTracker
+//
+//  Created by Jonathan Denney on 9/10/24.
+//
+
+import Foundation
+
+struct Exchange {
+    let symbol: String
+    let rate: Double
+}
