8af68e11915f40a67ddaf06a824f883f48408591
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-10 12:56:51 -0500

    Initial Commit

---
+++ BTCTracker/BTCTracker.h
@@ -0,0 +1,18 @@
+//
+//  BTCTracker.h
+//  BTCTracker
+//
+//  Created by Jonathan Denney on 9/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BTCTracker.
+FOUNDATION_EXPORT double BTCTrackerVersionNumber;
+
+//! Project version string for BTCTracker.
+FOUNDATION_EXPORT const unsigned char BTCTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTCTracker/PublicHeader.h>
+
+
