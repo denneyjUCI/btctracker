157a75840f3834406ba07e3a9501f04457efd5ec
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-10 15:35:32 -0500

    Throw badRequest error on 4xx

--- BTCTrackerTests/BinanceExchangeMapperTests.swift
+++ BTCTrackerTests/BinanceExchangeMapperTests.swift
@@ -8,11 +8,15 @@
 import XCTest
 
 final class BinanceExchangeMapper {
-    enum Error: Swift.Error {
+    public enum Error: Swift.Error {
         case invalidData
+        case badRequest
     }
 
     static func map(_ data: Data, from response: HTTPURLResponse) throws {
+        if [400, 403, 409, 418].contains(response.statusCode) {
+            throw Error.badRequest
+        }
         throw Error.invalidData
     }
 }
@@ -31,10 +35,26 @@ final class BinanceExchangeMapperTests: XCTestCase {
         XCTAssertThrowsError(try BinanceExchangeMapper.map(data, from: anyHTTPURLResponse()))
     }
 
+    func test_map_with4xxResponseCodeAndValidData_throwsBadRequestError() {
+        let data = Data("{ \"symbol\": \"BTCUSDT\", \"price\": 200.0 }".utf8)
+
+        let samples = [400, 403, 409, 418]
+        samples.forEach { code in
+            let result = Result { try BinanceExchangeMapper.map(data, from: anyHTTPURLResponse(code: code)) }
+
+            switch result {
+            case .failure(let error as BinanceExchangeMapper.Error):
+                XCTAssertEqual(error, BinanceExchangeMapper.Error.badRequest)
+            default:
+                XCTFail("Expected bad request error, got \(result) instead")
+            }
+        }
+    }
+
     // MARK: - Helpers
-    private func anyHTTPURLResponse() -> HTTPURLResponse {
+    private func anyHTTPURLResponse(code: Int = 200) -> HTTPURLResponse {
         let url = URL(string: "http://any-url.com")!
-        return HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return HTTPURLResponse(url: url, statusCode: code, httpVersion: nil, headerFields: nil)!
     }
 
 }
