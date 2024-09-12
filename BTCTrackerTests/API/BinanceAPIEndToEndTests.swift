229b480565cd4d89cd3fe5e0cf2886477d32fa75
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-12 17:46:31 -0500

    Remove duplication from end-to-end-tests

--- BTCTrackerTests/API/BinanceAPIEndToEndTests.swift
+++ BTCTrackerTests/API/BinanceAPIEndToEndTests.swift
@@ -6,45 +6,43 @@ final class BinanceAPIEndToEndTests: XCTestCase {
     func test_load_inUS_withMainURL_doesNotLoadValuesSuccessfully() throws {
         let url = URL(string: "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")!
 
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let exp = expectation(description: "Wait for load")
-
-        client.get(request: .init(url: url)) { result in
-            switch result {
-            case let .success(values):
-                let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
-                XCTAssertNil(result)
-            default:
-                XCTFail("Expected successful load, got \(result) instead")
-            }
-
-            exp.fulfill()
+        let result = loadResult(from: url)
+        switch result {
+        case let .success(values):
+            let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
+            XCTAssertNil(result)
+        default:
+            XCTFail("Expected successful load, got \(result) instead")
         }
-
-        wait(for: [exp], timeout: 10)
     }
 
     func test_load_inUS_withUSExtension_loadsValuesSuccessfully() throws {
         let url = URL(string: "https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT")!
 
+        let result = loadResult(from: url)
+        switch result {
+        case let .success(values):
+            let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
+            XCTAssertNotNil(result)
+        default:
+            XCTFail("Expected successful load, got \(result) instead")
+        }
+    }
+
+    // MARK: - Helpers
+    private func loadResult(from url: URL) -> HTTPClient.Result {
         let session = URLSession(configuration: .ephemeral)
         let client = URLSessionHTTPClient(session: session)
         let exp = expectation(description: "Wait for load")
 
+        var capturedResult: HTTPClient.Result!
         client.get(request: .init(url: url)) { result in
-            switch result {
-            case let .success(values):
-                let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
-                XCTAssertNotNil(result)
-            default:
-                XCTFail("Expected successful load, got \(result) instead")
-            }
-
+            capturedResult = result
             exp.fulfill()
         }
 
         wait(for: [exp], timeout: 10)
+        return capturedResult
     }
 
 }
