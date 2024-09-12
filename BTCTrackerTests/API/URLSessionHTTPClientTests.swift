584e80a9e7a99044460ce347d9099cb1ac070a5c
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-12 15:31:31 -0500

    Fail with non-nil url responses and error

--- BTCTrackerTests/API/URLSessionHTTPClientTests.swift
+++ BTCTrackerTests/API/URLSessionHTTPClientTests.swift
@@ -52,11 +52,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getRequest_failsOnAllUnexpectedValuesRepresentations() {
         let anyData = Data("any".utf8)
         let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
 
         XCTAssertNotNil(resultErrorFor(data: nil, response: nil, error: nil))
         XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil))
         XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: nil))
         XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: anyNSError()))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: anyNSError()))
     }
 
     // MARK: - Helpers
