fcf2516a915619b224aae5abb404db0f341dcb30
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-12 14:09:40 -0500

    Get with request performs request with given URLRequest

--- BTCTrackerTests/API/URLSessionHTTPClientTests.swift
+++ BTCTrackerTests/API/URLSessionHTTPClientTests.swift
@@ -1,12 +1,99 @@
 import XCTest
 
 final class URLSessionHTTPClient {
-    
+    private let session: URLSession
+    init(session: URLSession) {
+        self.session = session
+    }
+    func get(request: URLRequest) {
+        session.dataTask(with: request, completionHandler: { _, _, _ in }).resume()
+    }
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    func test_canInit() throws {
-        let _ = URLSessionHTTPClient()
+    override func tearDown() {
+        super.tearDown()
+
+        URLProtocolStub.removeStub()
+    }
+
+    func test_getRequest_performsGETRequestWithProvidedRequest() throws {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHTTPClient(session: session)
+        let request = URLRequest(url: URL(string: "http://any-url.com")!)
+        let exp = expectation(description: "wait for request")
+        URLProtocolStub.observeRequests { req in
+            XCTAssertEqual(req.url, request.url)
+            XCTAssertEqual(req.httpMethod, request.httpMethod)
+            exp.fulfill()
+        }
+
+        sut.get(request: request)
+
+        wait(for: [exp], timeout: 1)
+    }
+
+    // MARK: - Helpers
+    private class URLProtocolStub: URLProtocol {
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+            let requestObserver: ((URLRequest) -> Void)?
+        }
+
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get { queue.sync { _stub } }
+            set { queue.sync { _stub = newValue }}
+        }
+
+        private static var queue = DispatchQueue(label: "URLProtocolStub.queue")
+
+        static func stub(data: Data?, response: URLResponse?, error: Error? = nil) {
+            stub = Stub(data: data, response: response, error: error, requestObserver: nil)
+        }
+
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
+        }
+
+        static func removeStub() {
+            stub = nil
+        }
+
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+
+        override func startLoading() {
+            guard let stub = URLProtocolStub.stub else { return }
+
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
+            if let error = stub.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            } else {
+                client?.urlProtocolDidFinishLoading(self)
+            }
+
+            stub.requestObserver?(request)
+        }
+
+        override func stopLoading() {}
+
     }
 }
