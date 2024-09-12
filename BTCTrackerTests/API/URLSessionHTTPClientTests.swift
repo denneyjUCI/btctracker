8179b1e2bb0ca64a948fe68b674e27d24757fecd
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-12 14:00:51 -0500

    Create URLSessionHTTPClient

---
+++ BTCTrackerTests/API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+final class URLSessionHTTPClient {
+    
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_canInit() throws {
+        let _ = URLSessionHTTPClient()
+    }
+}
