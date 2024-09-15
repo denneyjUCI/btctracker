6454554abfa3fa9d2330beda6828762922bf24ea
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-15 17:17:11 -0500

    Use CryptoCompare api url and mapper in end-to-end tests

--- BTCTrackerAPIEndToEndTests/CryptoCompareAPIEndToEndTests.swift
+++ BTCTrackerAPIEndToEndTests/CryptoCompareAPIEndToEndTests.swift
@@ -3,26 +3,13 @@ import BTCTracker
 
 final class CryptoCompareAPIEndToEndTests: XCTestCase {
 
-    func test_load_inUS_withMainURL_doesNotLoadValuesSuccessfully() throws {
-        let url = URL(string: "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")!
-
-        let result = loadResult(from: url)
-        switch result {
-        case let .success(values):
-            let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
-            XCTAssertNil(result)
-        default:
-            XCTFail("Expected successful load, got \(result) instead")
-        }
-    }
-
     func test_load_inUS_withUSExtension_loadsValuesSuccessfully() throws {
-        let url = URL(string: "https://api.binance.us/api/v3/ticker/price?symbol=BTCUSDT")!
+        let url = URL(string: "https://min-api.cryptocompare.com/data/generateAvg?fsym=BTC&tsym=USD&e=coinbase")!
 
         let result = loadResult(from: url)
         switch result {
         case let .success(values):
-            let result = try? BinanceExchangeMapper.map(values.0, from: values.1)
+            let result = try? CryptoCompareExchangeMapper.map(values.0, from: values.1)
             XCTAssertNotNil(result)
         default:
             XCTFail("Expected successful load, got \(result) instead")
