d4e0ebe6ccf113367e8ffa80b545ac4acc0ee575
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-17 16:58:00 -0500

    Send ticks more than once

--- BTCTracker/BTCTrackerTests/Timer Infra/TimerInfraTests.swift
+++ BTCTracker/BTCTrackerTests/Timer Infra/TimerInfraTests.swift
@@ -1,12 +1,17 @@
 import XCTest
 
 final class FoundationTimer {
+    private let timer: Timer
     private let tick: () -> Void
-    init(tick: @escaping () -> Void = {}) {
+    init(hertz: Int = 1, tick: @escaping () -> Void = {}) {
         self.tick = tick
+        timer = Timer.scheduledTimer(withTimeInterval: 1 / Double(hertz), repeats: true, block: { _ in
+            tick()
+        })
     }
 
     func start() {
+        RunLoop.main.add(timer, forMode: .common)
         tick()
     }
 }
@@ -28,6 +33,22 @@ final class TimerInfraTests: XCTestCase {
         sut.start()
 
         XCTAssertEqual(tickCount, 1)
+    }
+
+    func test_start_sendsTickAtInterval() {
+        var tickCount = 0
+        let exp = expectation(description: "wait for tick")
+        exp.expectedFulfillmentCount = 2
+
+        let sut = FoundationTimer(hertz: 1000, tick: {
+            tickCount += 1
+            exp.fulfill()
+        })
+        sut.start()
+
+        wait(for: [exp], timeout: 0.1)
+
+        XCTAssertEqual(tickCount, 2)
     }
 
 }
