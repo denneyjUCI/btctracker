6cea12acbe41ba4340e64a2149a17c9b68f4ab6b
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-17 16:51:28 -0500

    Send tick on start

--- BTCTracker/BTCTrackerTests/Timer Infra/TimerInfraTests.swift
+++ BTCTracker/BTCTrackerTests/Timer Infra/TimerInfraTests.swift
@@ -1,7 +1,14 @@
 import XCTest
 
 final class FoundationTimer {
+    private let tick: () -> Void
+    init(tick: @escaping () -> Void = {}) {
+        self.tick = tick
+    }
 
+    func start() {
+        tick()
+    }
 }
 
 final class TimerInfraTests: XCTestCase {
@@ -12,6 +19,15 @@ final class TimerInfraTests: XCTestCase {
         let _ = FoundationTimer()
 
         XCTAssertEqual(tickCount, 0)
+    }
+
+    func test_start_sendsTick() {
+        var tickCount = 0
+        let sut = FoundationTimer(tick: { tickCount += 1})
+
+        sut.start()
+
+        XCTAssertEqual(tickCount, 1)
     }
 
 }
