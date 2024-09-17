844271b21119911a4f140a8aeafc50232b2643e4
Jonathan Denney <jonathan.denney1@gmail.com>
2024-09-16 21:25:25 -0500

    Add iOS framework

---
+++ BTCTracker/BTCTrackeriOS/BTCTrackeriOS.h
@@ -0,0 +1,18 @@
+//
+//  BTCTrackeriOS.h
+//  BTCTrackeriOS
+//
+//  Created by Jonathan Denney on 9/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BTCTrackeriOS.
+FOUNDATION_EXPORT double BTCTrackeriOSVersionNumber;
+
+//! Project version string for BTCTrackeriOS.
+FOUNDATION_EXPORT const unsigned char BTCTrackeriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTCTrackeriOS/PublicHeader.h>
+
+
---
+++ BTCTracker/BTCTrackeriOSTests/BTCTrackeriOSTests.swift
@@ -0,0 +1,15 @@
+//
+//  BTCTrackeriOSTests.swift
+//  BTCTrackeriOSTests
+//
+//  Created by Jonathan Denney on 9/16/24.
+//
+
+import XCTest
+@testable import BTCTrackeriOS
+
+final class BTCTrackeriOSTests: XCTestCase {
+
+    
+
+}
